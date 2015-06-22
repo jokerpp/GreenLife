c03f10d59406121472b9e9e906e3b000b9c580c1
ping <978231142@qq.com>
2015-06-22 18:12:26 +0800

    src 提交

--- GreenLife/src/cn/crane/application/greenlife/ui/FragmentMy.java
+++ GreenLife/src/cn/crane/application/greenlife/ui/FragmentMy.java
@@ -1,7 +1,13 @@
 package cn.crane.application.greenlife.ui;
 
+
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 import cn.crane.application.greenlife.R;
 import cn.crane.framework.fragment.BaseFragment;
+import cn.crane.framework.utils.myaccount.PullToZoomScrollViewEx;
 
 /**
  * @author Ruifeng.yu E-mail:xyyh0116@aliyun.com
@@ -9,7 +15,7 @@ import cn.crane.framework.fragment.BaseFragment;
  * 
  */
 public class FragmentMy extends BaseFragment {
-
+	private PullToZoomScrollViewEx scrollView;
 	@Override
 	protected int getLayoutId() {
 		// TODO Auto-generated method stub
@@ -19,18 +25,42 @@ public class FragmentMy extends BaseFragment {
 	@Override
 	protected void findViews() {
 		// TODO Auto-generated method stub
-
+		this.loadViewForCode();
+		scrollView = (PullToZoomScrollViewEx) this
+				.findViewById(R.id.scroll_view);
 	}
 
 	@Override
 	protected void bindViews() {
 		// TODO Auto-generated method stub
+		DisplayMetrics localDisplayMetrics = new DisplayMetrics();
+		getActivity().getWindowManager().getDefaultDisplay().getMetrics(localDisplayMetrics);
+		int mScreenHeight = localDisplayMetrics.heightPixels;
+		int mScreenWidth = localDisplayMetrics.widthPixels;
 
+		LinearLayout.LayoutParams localObject = new LinearLayout.LayoutParams(
+				mScreenWidth, (int) (9.0F * (mScreenWidth / 16.0F)));
+
+		scrollView.setHeaderLayoutParams(localObject);
 	}
 
 	@Override
 	protected void init() {
 		// TODO Auto-generated method stub
+
+	}
+	
+	private void loadViewForCode() {
+		PullToZoomScrollViewEx scrollView = (PullToZoomScrollViewEx) findViewById(R.id.scroll_view);
+		View headView = LayoutInflater.from(getActivity()).inflate(
+				R.layout.profile_head_view, null, false);
+		View zoomView = LayoutInflater.from(getActivity()).inflate(
+				R.layout.profile_zoom_view, null, false);
+		View conentView = LayoutInflater.from(getActivity()).inflate(
+				R.layout.profile_content_view, null, false);
+		scrollView.setHeaderView(headView);
+		scrollView.setZoomView(zoomView);
+		scrollView.setScrollContentView(conentView);
 
 	}
 
